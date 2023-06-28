049bd99ce8933e54da4ef45506d8a8efe7123a6d
drwadu <dominik.rusovac@hotmail.com>
2023-06-28 12:08:06 +0200

    update.

---
+++ experiments/scripts/hpc-fdp/23_2_0.5_0.575_0.9_0.3_1000_3_300.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=p_jury
+#SBATCH --partition=haswell
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=168:00:00
+#SBATCH --mail-type=end
+#SBATCH --mail-user=doru415d@tu-dresden.de
+#SBATCH --job-name=23_2_0.5_0.575_0.9_0.3_1000_3_300
+srun ./simcjt 2 0.5 0.575 0.9 0.3 1000 3 300 > out-fdp/23_2_0.5_0.575_0.9_0.3_1000_3_300.out
---
+++ experiments/scripts/hpc-fdp/39_2_0.5_0.975_0.9_0.3_1000_3_300.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=p_jury
+#SBATCH --partition=haswell
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=168:00:00
+#SBATCH --mail-type=end
+#SBATCH --mail-user=doru415d@tu-dresden.de
+#SBATCH --job-name=39_2_0.5_0.975_0.9_0.3_1000_3_300
+srun ./simcjt 2 0.5 0.975 0.9 0.3 1000 3 300 > out-fdp/39_2_0.5_0.975_0.9_0.3_1000_3_300.out
